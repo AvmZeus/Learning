a56185ec5fe789162fba568418b7776e11dc83f1
ofdrykkja <sweetrenard@gmail.com>
2021-06-27 19:09:13 +0300

    add main source file

---
+++ app/src/main/java/com/example/cleannotes/Main.kt
@@ -0,0 +1,11 @@
+package com.example.cleannotes
+
+class Note(val title: String, val text: String)
+
+fun createNote(title: String, text: String): Note = Note(title, text)
+
+val condition = false
+
+fun main() {
+
+}
